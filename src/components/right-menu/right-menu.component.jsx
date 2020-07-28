0f6b2819ce7cc353d907102694efbcc587994c18
Mikky <mahmoudothman.dev@gmail.com>
2020-07-28 20:21:05 +0200

    my project

--- src/components/right-menu/right-menu.component.jsx
+++ src/components/right-menu/right-menu.component.jsx
@@ -23,32 +23,59 @@ const RightMenu = () => {
 
       <div className={`background ${hiddenState}`}>
         <div className="list">
-          <Link to="/shop/living-room" className={`link ${selected} `}>
-            <span className="name"> LIVING ROOM</span>
+          <Link
+            to="/shop/living-room"
+            className={`link ${selected} `}
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
+            <span className="name">LIVING ROOM</span>
             <span className="icon-living-room icon"></span>
           </Link>
-          <Link to="/shop/office" className="link">
+          <Link
+            to="/shop/office"
+            className="link"
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
             <span className="name"> OFFICE</span>
             <span className="icon-office icon"></span>
           </Link>
-          <Link to="/shop/bed-room" className="link">
+          <Link
+            to="/shop/bed-room"
+            className="link"
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
             <span className="name"> BED ROOM</span>
             <span className="icon-bed-room icon"></span>
           </Link>
-          <Link to="/shop/for-kids" className="link">
+          <Link
+            to="/shop/for-kids"
+            className="link"
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
             <span className="name"> FOR KIDS</span>
             <span className="icon-for-kids icon "></span>
           </Link>
-          <Link to="/shop/kitchen" className="link">
+          <Link
+            to="/shop/kitchen"
+            className="link"
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
             <span className="name"> KITCHEN</span>
             <span className="icon-kitchen icon"></span>
           </Link>
-          <Link to="/shop/accessories" className="link">
+          <Link
+            to="/shop/accessories"
+            className="link"
+            onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+          >
             <span className="name"> ACCESSORIES</span>
             <span className="icon-accessories icon"></span>
           </Link>
         </div>
-        <Link to="/shop">
+        <Link
+          to="/shop"
+          onClick={() => (window.innerWidth <= 425 ? toggle() : null)}
+        >
           <GoTo cheldern="SHOW ALL CATEGORIES" />
         </Link>
       </div>
