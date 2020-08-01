82359575a853563727e9d9ec4d867243eff0b801
Mikky <mahmoudothman.dev@gmail.com>
2020-08-01 16:51:39 +0200

    my pro

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -36,6 +37,13 @@ const CheckoutPage = ({ cartItem, total }) => (
     ))}
 
     <div className="total"> ToTAL: ${total}</div>
+
+    <div className="test-warning">
+      *please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - exp: 3/31 - cvv: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
